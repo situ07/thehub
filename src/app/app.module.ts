1425e00eadf3eb454a5cca081e8d89fd153f059b
Shitanshu Shekhar <shitanshus@mindfiresolutions.com>
2018-11-23 17:00:14 +0530

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RouterModule } from '@angular/router';
@@ -14,6 +17,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,MatCardModule,MatInputModule,
     RouterModule.forRoot([
       { path: '', component: LoginComponent }
     ])
